2a744916ce53f537ced74ed0e88ba2bc7640939e
CosmologicalButter <53821637+Normolo@users.noreply.github.com>
2021-05-11 18:58:31 +0200

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,37 @@
+from random import randbytes
+import time
+cli = True
+
+print('░█████╗░░░░░░░░░░░░░░░  ██╗███╗░░██╗░██████╗████████╗░█████╗░██╗░░░░░██╗░░░░░███████╗██████╗░')
+print('██╔══██╗░░██╗░░░░██╗░░  ██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██║░░░░░██║░░░░░██╔════╝██╔══██╗')
+print('██║░░██║██████╗██████╗  ██║██╔██╗██║╚█████╗░░░░██║░░░███████║██║░░░░░██║░░░░░█████╗░░██████╔╝')
+print('██║░░██║╚═██╔═╝╚═██╔═╝  ██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║██║░░░░░██║░░░░░██╔══╝░░██╔══██╗')
+print('╚█████╔╝░░╚═╝░░░░╚═╝░░  ██║██║░╚███║██████╔╝░░░██║░░░██║░░██║███████╗███████╗███████╗██║░░██║')
+print('░╚════╝░░░░░░░░░░░░░░░  ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝╚══════╝╚═╝░░╚═╝')
+time.sleep(2)
+print('Welcome to the O++ Installer')
+print('You will now be entering a Command Line Interface in order to install O++')
+while cli == True:
+    clir = input('OPP//: : ')
+    if clir == 'exit':
+        print('Exiting')
+        raise(SystemExit)
+    if clir == 'help':
+        print('Command List')
+        print('help - this command')
+        print('exit - exits opp installer')
+        print('install - installs opp')
+    if clir == 'install':
+        print("[                    ] 0% ")
+        time.sleep(5)
+        print("[=====               ] 25%")
+        time.sleep(5)
+        print("[==========          ] 50%")
+        time.sleep(5)
+        print("[===============     ] 75%")
+        time.sleep(5)
+        print("[====================] 100%")
+        time.sleep(3)
+        print('Succesfully installed!')
+        print('Enter exit on CLI to exit')
+    
