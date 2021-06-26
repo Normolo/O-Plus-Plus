d507e3c7ba9523cc773621756c9a7afca94b3bd5
CosmologicalButter <53821637+Normolo@users.noreply.github.com>
2021-06-26 09:12:09 +0200

    Create auto_sorter.lua

---
+++ templates/roblox_tools/auto_sorter.lua
@@ -0,0 +1,18 @@
+opp.start.script
+
+if toolbox.item.new(added):
+    if toolbox.item.new(par{has.script.lua=true}):
+        script.lua = script
+        search[script(SSTGSearch) for {'game:GetService("ServerStorage")'}]
+        search[script(SSSearch) for {'game:GetService("ServerScriptService")'}]
+        if SSSearch = true:
+            clone[script.lua][game/ServerScriptService].par({w/name=cloned_script.lua})
+            del[script.lua]
+        if SSTGSearch = true:
+            clone[script.lua][game/ServerStorage].par({w/name=clones_script.lua})
+            del[script.lua]
+        if SSTGSearch = true:
+            if SSSearch = true:
+                output['Script must be sorted manually due to multile error_code']
+
+opp.conclude.script(0)
